b032e6256dd087f7ce98950c222c5085c2d5a430
Kim geonhee <kkoni13@korea.ac.kr>
2025-05-29 13:10:24 +0900

    11

--- assignment6/Run Sample 2/Assets/Scripts/Character.cs
+++ assignment6/Run Sample 2/Assets/Scripts/Character.cs
@@ -12,37 +12,42 @@ public class Character : MonoBehaviour
     void Awake()
     {
         GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+        RemainJump = MaxJump;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // 좌클릭시 RemainJump를 하나 소모하여 CharacterJumpPower의 힘으로 점프한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if (RemainJump > 0 && Input.GetMouseButtonDown(0))
+        {
+            RemainJump--;
+            Jump(CharacterJumpPower);
+        }
     }
 
-    // Jump with power
     void Jump(float power)
     {
-        GetComponent<Rigidbody2D>().AddForce(new Vector3(0, CharacterJumpPower, 0), ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+        GetComponent<Rigidbody2D>().AddForce(new Vector3(0, power, 0), ForceMode2D.Impulse);
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        // tag가 Platform인 것과 충돌하면 RemainJump를 초기화한다.
-        // tag가 Obstacle인 것과 충돌하면 게임 오버한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if (col.gameObject.CompareTag("Platform"))
+        {
+            RemainJump = MaxJump;
+        }
+        else if (col.gameObject.CompareTag("Obstacle"))
+        {
+            GM.GameOver();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        // tag가 Point인 것과 충돌하면 Point를 하나 얻고, 충돌한 오브젝트를 삭제한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if (col.gameObject.CompareTag("Point"))
+        {
+            GM.GetPoint(1);
+            Destroy(col.gameObject);
+        }
     }
 }
--- assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
+++ assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject Character;
     public GameObject CamObj;
-    
+
     const float CharacterSpeed = 3f;
 
     public int NowScore = 0;
@@ -19,14 +19,12 @@ public class GameManager : MonoBehaviour
         MyUIManager.DisplayScore(NowScore);
         MyUIManager.DisplayMessage("", 0);
     }
-    
-    // Update is called once per frame
+
     void Update()
     {
         MoveCharacter();
     }
 
-    // For smooth cam moving, it's good to use LateUpdate.
     void LateUpdate()
     {
         MoveCam();
@@ -34,37 +32,36 @@ public class GameManager : MonoBehaviour
 
     void MoveCam()
     {
-        // CamObj는 Character의 x, y position을 따라간다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if (Character && CamObj)
+        {
+            Vector3 pos = CamObj.transform.position;
+            pos.x = Character.transform.position.x;
+            pos.y = Character.transform.position.y;
+            CamObj.transform.position = pos;
+        }
     }
 
     void MoveCharacter()
     {
-        // Character는 초당 CharacterSpeed의 속도로 우측으로 움직인다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if (Character)
+        {
+            Character.transform.position += Vector3.right * CharacterSpeed * Time.deltaTime;
+        }
     }
 
     public void GameOver()
     {
-        // Character를 삭제하고, "Game Over!"라는 메시지를 3초간 띄우고, RestartButton을 활성화한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        Destroy(Character);
+        MyUIManager.DisplayMessage("Game Over!", 3f);
+        MyUIManager.RestartButton.SetActive(true);
     }
 
     public void GetPoint(int point)
     {
-        // point만큼 점수를 증가시키고 UI에 표시한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        NowScore += point;
+        MyUIManager.DisplayScore(NowScore);
     }
 
-    // Restart the game
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
